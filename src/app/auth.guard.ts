ae9f2a5f1ad065a7faf469b605d394e58767e592
snktkdm712 <31816741+snktkdm712@users.noreply.github.com>
2021-02-02 17:10:17 +0530

    Add files via upload

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginComponent } from './login/login.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  a:LoginComponent;
+ 
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.a.isLoggedIn()) {
+        return true;
+      }else{
+        return false;
+      }
+  }
+  
+}
